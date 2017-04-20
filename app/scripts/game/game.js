be9320b6460a3c0bf4fc4894406045613aeb25f4
Dario Coco <dariococo@MacBook-Pro-di-Dario.local>
2017-04-20 17:21:46 +0200

    Drawing X and O on grid basing on current player

--- app/scripts/game/game.js
+++ app/scripts/game/game.js
@@ -1,5 +1,15 @@
 angular.module('Game', [])
 .service('GameManager', function() {
-  this.grid = [['X','O','X'],['X','O','X'],['X','O','X']]
-  this.newGame = function() {}
+  this.grid = [['','',''],['','',''],['','','']]
+  this.player = 'X'
+  this.update = function($row, $col) {
+  	this.grid[$row][$col]= this.swap_player()
+  }
+  this.swap_player = function(){
+  	if (this.player == 'X')
+  	  this.player = 'O'
+  	else
+      this.player = 'X'
+    return this.player
+  }
 })
